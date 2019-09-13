0266947aa0042043100a1be55f3c4cde9f3c66b5
Kibrael Roell <kibrael.roell@cfpb.gov>
2019-09-13 15:07:21 -0400

    capture changes

---
+++ sql/s2155_lar_counts_by_county.sql
@@ -0,0 +1,21 @@
+﻿SELECT 
+	CONCAT(state, county),
+	COUNT(*)
+FROM s3_out.lar_2018_07_24
+WHERE 
+	CONCAT(agency_code, respondent_id) IN ( 
+		SELECT
+			CONCAT(agency_code, respondent_id)
+		FROM 
+			s3_out.lar_2018_07_24
+		GROUP BY 
+			CONCAT(agency_code, respondent_id)
+		HAVING 
+			COUNT(*) >=500) AND
+	CONCAT(state,county) NOT LIKE '%NA%' AND
+	state != '' AND 
+	county != ''
+GROUP BY CONCAT(state, county)
+ORDER BY CONCAT(state, county) ASC
+
+  
